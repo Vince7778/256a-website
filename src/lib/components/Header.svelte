98111c6960db0d2ecbe331aad5ba8500f81c4c2b
Conor Kennedy <realvince7778@gmail.com>
2024-09-28 21:26:19 -0700

    basic scaffolding

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+    
+</script>
+
+<div class="header">
+    <div class="title"><a href="/" class="hidden-a">Music 256A: Conor Kennedy</a></div>
+</div>
+
+<style>
+    .header {
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        gap: 5px;
+
+        border-bottom: 1px solid var(--fg-color);
+        padding: 5px;
+        margin: 5px 5px 10px 5px;
+    }
+
+    .title {
+        font-size: 2em;
+        font-weight: bold;
+    }
+</style>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import Header from "$lib/components/Header.svelte";
+    import "$lib/styles.css";
+
+</script>
+
+<Header />
+<div style="margin: 15px">
+    <slot></slot>
+</div>
