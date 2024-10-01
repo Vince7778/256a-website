103eb52a5b81aa0fc732b80292cdc29848fcd025
Conor Kennedy <realvince7778@gmail.com>
2024-09-30 20:27:39 -0700

    section

---
+++ src/lib/components/Section.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+    export let title: string;
+</script>
+
+<div class="section">
+    <div class="section-title">{title}</div>
+    <div class="section-body">
+        <slot></slot>
+    </div>
+</div>
+
+<style>
+    .section-title {
+        font-size: 1.5em;
+        font-weight: bold;
+        margin-bottom: 5px;
+    }
+
+    .section-body {
+        margin-left: 10px;
+        padding: 5px 0px 5px 15px;
+        border-left: 2px solid var(--fg-color);
+    }
+
+    @media only screen and (min-width: 720px) {
+        .section-body {
+            max-width: 70%;
+        }
+    }
+</style>
--- src/routes/hw1/+page.svelte
+++ src/routes/hw1/+page.svelte
@@ -1,1 +1,15 @@
-<p>Hello!</p>
+<script>
+	import Section from "$lib/components/Section.svelte";
+
+</script>
+
+<svelte:head>
+    <title>HW1</title>
+</svelte:head>
+
+<Section title="Part 1: Reading Response">
+    Hello, world! I'm going to write some more text in order to test the width bounds of this thing. I mean, it shouldn't be too long after all, right?
+    And do line breaks count as new paragraphs?
+
+    How do I get new paragraphs?
+</Section>
